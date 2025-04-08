b42e2507ff81e09adad54328bd8e6f1e0139217d
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-04-08 14:28:49 +0530

    [Praveen] ADD: Search name get Records

--- src/main/java/com/bridgeLabz/SearchRecords.java
+++ src/main/java/com/bridgeLabz/SearchRecords.java
@@ -10,7 +10,7 @@ public class SearchRecords {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         String file = "C:\\Users\\Piyush\\OneDrive\\Desktop\\bridgelabz-workspace\\JAVA_IO\\CSV-data\\src\\main\\java\\com\\bridgeLabz\\employee.csv";
-        try(CSVReader reader = new CSVReader(new FileReader(file))) {
+        try(CSVReader reader = new CSVReader(new FileReader(file))) { 
             String[] line;
             line = reader.readNext();
             System.out.printf("%-5s %10s%n",line[2],line[3]);
